514a1a2e4bd7a184ad4df94699236405995eeda6
grahamsawers <grahamsawers@gmail.com>
2019-11-03 15:44:35 +0000

    Add simple api fetch.

--- client/src/components/AdvertContainer.js
+++ client/src/components/AdvertContainer.js
@@ -1,54 +1,53 @@
 import React from 'react';
-import AdvertList from './AdvertList.js'
+import AdvertList from './AdvertList.js';
 
 class AdvertContainer extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            adverts: [
-                {
-                    id: 0,
-                    seller: "Jordan",
-                    title: "chocolate",
-                    price: 2000
-
-                },
-                {
-                    id: 1,
-                    seller: "Allan",
-                    title: "iPhonw",
-                    price: 70000
-
-                },
-                {
-                    id: 2,
-                    seller: "Shauna",
-                    title: "table",
-                    price: 5000
-
-                },
-                {
-                    id: 3,
-                    seller: "Graham",
-                    title: "burger",
-                    price: 1000
-
-                }
-            ]
+  constructor() {
+    super();
+    this.state = {
+      adverts: [
+        {
+          id: 0,
+          seller: 'Jordan',
+          title: 'chocolate',
+          price: 2000
+        },
+        {
+          id: 1,
+          seller: 'Allan',
+          title: 'iPhonw',
+          price: 70000
+        },
+        {
+          id: 2,
+          seller: 'Shauna',
+          title: 'table',
+          price: 5000
+        },
+        {
+          id: 3,
+          seller: 'Graham',
+          title: 'burger',
+          price: 1000
         }
-    }
+      ]
+    };
+  }
 
+  componentDidMount() {
+    fetch('http://localhost:8080/adverts')
+      .then(res => res.json())
+      .then(result => this.setState({ adverts: result }));
+  }
 
-    render() {
-        return (
-            <>
-                <div>I am the AdvertContainer!</div>
-                <AdvertList adverts={this.state.adverts} />
-            </>
-        )
-    }
-
+  render() {
+    return (
+      <>
+        <div>I am the AdvertContainer!</div>
+        <AdvertList adverts={this.state.adverts} />
+      </>
+    );
+  }
 }
-
 
 export default AdvertContainer;
