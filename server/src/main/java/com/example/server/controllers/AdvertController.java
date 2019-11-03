18493b1e249f9829d551601d53c86437a51657ce
Allan Milne <amilne.wd@gmail.com>
2019-11-03 15:45:11 +0000

    find Advert and Seller by id

--- server/src/main/java/com/example/server/controllers/AdvertController.java
+++ server/src/main/java/com/example/server/controllers/AdvertController.java
@@ -1,10 +1,15 @@
 package com.example.server.controllers;
 
+import com.example.server.models.Advert;
 import com.example.server.repositories.AdvertRepository;
 import com.example.server.repositories.SellerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/adverts")
@@ -16,4 +21,8 @@ public class AdvertController {
     @Autowired
     SellerRepository sellerRepository;
 
+    @GetMapping(value = "/{id}")
+    public Optional<Advert> getAdvert(@PathVariable Long id){
+        return advertRepository.findById(id);
+    }
 }
--- server/src/main/java/com/example/server/controllers/SellerController.java
+++ server/src/main/java/com/example/server/controllers/SellerController.java
@@ -1,10 +1,15 @@
 package com.example.server.controllers;
 
+import com.example.server.models.Seller;
 import com.example.server.repositories.AdvertRepository;
 import com.example.server.repositories.SellerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/sellers")
@@ -15,5 +20,10 @@ public class SellerController {
 
     @Autowired
     SellerRepository sellerRepository;
+
+    @GetMapping(value = "/{id}")
+    public Optional<Seller> getSeller(@PathVariable Long id){
+        return sellerRepository.findById(id);
+    }
 
 }
