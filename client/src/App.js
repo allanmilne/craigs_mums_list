1c6099c688bd76dcb238bc0b8d994d4c4ed26598
Jordan Smith <jordsmithy@gmail.com>
2019-11-03 14:50:38 +0000

    add map for adverts in advert list, adding in single advert component"

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
+import AdvertContainer from './components/AdvertContainer'
 
 function App() {
   return (
-    <div className="App">
-        Hello im the app
-    </div>
+    <AdvertContainer />
   );
 }
 
---
+++ client/src/components/AdvertList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class AdvertList extends React.Component {
+
+    render() {
+        return(
+            <ul>
+            {this.props.adverts.map((advert) => (
+                <li key={advert.id}>
+                    <Advert advert={advert}/>
+                </li>
+            ))}
+            </ul>
+        )
+    }
+}
+
+
+export default AdvertList
