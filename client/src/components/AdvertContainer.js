33ea4fb56a4a5d222858b7d65913a140f47ea5a4
grahamsawers <grahamsawers@gmail.com>
2019-11-03 16:10:48 +0000

    Update fetch request to match api /adverts.

--- client/src/components/AdvertContainer.js
+++ client/src/components/AdvertContainer.js
@@ -5,39 +5,16 @@ class AdvertContainer extends React.Component {
   constructor() {
     super();
     this.state = {
-      adverts: [
-        {
-          id: 0,
-          seller: 'Jordan',
-          title: 'chocolate',
-          price: 2000
-        },
-        {
-          id: 1,
-          seller: 'Allan',
-          title: 'iPhonw',
-          price: 70000
-        },
-        {
-          id: 2,
-          seller: 'Shauna',
-          title: 'table',
-          price: 5000
-        },
-        {
-          id: 3,
-          seller: 'Graham',
-          title: 'burger',
-          price: 1000
-        }
-      ]
+      adverts: []
     };
   }
 
   componentDidMount() {
     fetch('http://localhost:8080/adverts')
       .then(res => res.json())
-      .then(result => this.setState({ adverts: result }));
+      .then(result => {
+        this.setState({ adverts: result._embedded.adverts });
+      });
   }
 
   render() {
