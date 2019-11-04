baacecdf19f1bffe43bdd7d58c0b557f33ee6e7d
Shona McKenzie <shona_mckenzie@hotmail.com>
2019-11-04 16:02:11 +0000

    added additional fields for diaply on advertdetail

--- client/src/components/AdvertDetail.js
+++ client/src/components/AdvertDetail.js
@@ -5,9 +5,14 @@ const AdvertDetail = ({ selectedAdvert }) => {
   return (
     <div>
       <p>{selectedAdvert.title}</p>
-      <p>{selectedAdvert.description}</p>
-      <p>{selectedAdvert.price}</p>
-      <p>{selectedAdvert._embedded.seller.location}</p>
+      <p>{selectedAdvert.image}</p>
+      <p>Description: {selectedAdvert.description}</p>
+      <p>Price: £{selectedAdvert.price}</p>
+      <p>Date Listed: {selectedAdvert.dateListed}</p>
+      <p>Category: {selectedAdvert.category}</p>
+      <p>Location: {selectedAdvert._embedded.seller.location}</p>
+      <p>Contact: {selectedAdvert._embedded.seller.firstName}</p>
+      <p>Phone: {selectedAdvert._embedded.seller.phoneNumber}</p>
     </div>
   );
 };
