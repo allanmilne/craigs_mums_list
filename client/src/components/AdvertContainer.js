34ebec1c037f1209f2fdf64f12d6cb52ac112264
Shona McKenzie <shona_mckenzie@hotmail.com>
2019-11-04 12:47:23 +0000

    flesh out advert detail component

--- client/src/components/AdvertContainer.js
+++ client/src/components/AdvertContainer.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import AdvertList from './AdvertList.js';
+import AdvertDetail from './AdvertDetail.js';
 
 class AdvertContainer extends React.Component {
   constructor() {
     super();
     this.state = {
       adverts: [],
-      selectedAdvert: null
+      selectedAdvert: ''
     };
   }
 
@@ -19,16 +20,18 @@ class AdvertContainer extends React.Component {
   }
 
   handleClick = (id) => {
+    const advert = this.state.adverts.find(advert => advert.id === parseInt(id));
     this.setState({
-      selectedAdvert: id
+      selectedAdvert: advert
     })
   }
 
   render() {
     return (
       <>
-        <div>I am the AdvertContainer!</div>
+        <div>Adverts</div>
         <AdvertList adverts={this.state.adverts} handleClick={this.handleClick}/>
+        <AdvertDetail selectedAdvert={this.state.selectedAdvert}/>
       </>
     );
   }
--- client/src/components/AdvertDetail.js
+++ client/src/components/AdvertDetail.js
@@ -1,8 +1,13 @@
 import React from 'react'
 
-const AdvertDetail = ({}) => {
+const AdvertDetail = ({selectedAdvert}) => {
   return(
-
+    <div>
+      <p>{selectedAdvert.title}</p>
+      <p>{selectedAdvert.description}</p>
+      <p>{selectedAdvert.price}</p>
+      {/* <p>{selectedAdvert.seller.location}</p> */}
+    </div>
   )
 }
 
