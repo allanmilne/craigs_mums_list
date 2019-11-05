c959f80363245b35dcdc36abc0fe976b0fcf76e5
grahamsawers <grahamsawers@gmail.com>
2019-11-05 11:40:15 +0000

    Add route to return adverts by seller id.

--- server/src/main/java/com/example/server/repositories/AdvertRepository.java
+++ server/src/main/java/com/example/server/repositories/AdvertRepository.java
@@ -1,9 +1,13 @@
 package com.example.server.repositories;
 
 import com.example.server.models.Advert;
+import com.example.server.models.Seller;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface AdvertRepository extends JpaRepository<Advert, Long> {
 
+    List<Advert> findAdvertBySellerId(Long sellerId);
 
 }
