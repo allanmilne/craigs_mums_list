4863e4fd388a9b103e5894225de7e2e96215155b
Shona McKenzie <shona_mckenzie@hotmail.com>
2019-11-06 14:16:29 +0000

    added placeholder for no image

--- client/src/components/advert/Advert.js
+++ client/src/components/advert/Advert.js
@@ -5,7 +5,7 @@ const Advert = ({ advert, handleClick }) => {
   return (
     <>
       <div className="card">
-        <img src={`/images/${advert.image}`} alt="" />
+        <img src={`/images/${advert.image}`} alt="Image coming soon   ᗧ···ᗣ···ᗣ··" />
         <div className="card-body">
           <p><b>Item Title: </b> {advert.title}</p>
           <p><b>Item Price: </b> £{advert.price}</p>
