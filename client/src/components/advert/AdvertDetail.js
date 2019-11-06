17a31253995b7f7377eb92359933adb9ca8af565
Jordan Smith <jordsmithy@gmail.com>
2019-11-06 12:52:43 +0000

    conflicts

--- client/src/components/advert/AdvertDetail.js
+++ client/src/components/advert/AdvertDetail.js
@@ -36,28 +36,9 @@ const AdvertDetail = ({ selectedAdvert }) => {
     )
 
   return (
-<<<<<<< HEAD
     <div className="modal" id="exampleModal" tabIndex="-1" role="dialog">
       <div className="modal-dialog" role="document">
         {modalContent}
-=======
-    <div class="container selected-advert">
-      <div class="row">
-        <div class="col">
-          {/* <img src={selectedAdvert.image}></img> */}
-          <img src={`/images/${selectedAdvert.image}`}/>
-        </div>
-        <div class="col">
-          <p>{selectedAdvert.title}</p>
-          <p>Description: {selectedAdvert.description}</p>
-          <p>Price: £{selectedAdvert.price}</p>
-          <p>Date Listed: {selectedAdvert.dateListed}</p>
-          <p>Category: {selectedAdvert.category}</p>
-          <p>Location: {selectedAdvert._embedded.seller.location}</p>
-          <p>Contact: {selectedAdvert._embedded.seller.firstName}</p>
-          <p>Phone: {selectedAdvert._embedded.seller.phoneNumber}</p>
-        </div>
->>>>>>> develop
       </div>
     </div>
   );
