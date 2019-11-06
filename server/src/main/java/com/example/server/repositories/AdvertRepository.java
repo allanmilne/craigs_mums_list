c8e536dabaa653f48ad6cf5fa3797d84d014f1f5
Allan Milne <amilne.wd@gmail.com>
2019-11-06 10:58:38 +0000

    case insensitive title search

--- server/src/main/java/com/example/server/repositories/AdvertRepository.java
+++ server/src/main/java/com/example/server/repositories/AdvertRepository.java
@@ -2,7 +2,6 @@ package com.example.server.repositories;
 
 import com.example.server.models.Advert;
 import com.example.server.models.Category;
-import com.example.server.models.Seller;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -13,6 +12,6 @@ public interface AdvertRepository extends JpaRepository<Advert, Long> {
 
     List<Advert> findAdvertBySellerId(Long sellerId);
 
-    @Query("select a from Advert a join a.seller s where s.id <> :id and a.title like %:title% and a.category = :category")
-    List<Advert> customSearch(@Param("id") Long id, @Param("title") String title, @Param("category") Category category);
+    @Query(value = "select a from Advert a join a.seller s WHERE s.id <> :id and lower(a.title) like lower(concat('%', :title, '%')) and a.category = :category")
+    List<Advert> customSearchIgnoreCase(@Param("id") Long id, @Param("title") String title, @Param("category") Category category);
 }
